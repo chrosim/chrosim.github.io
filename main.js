affdfa0e7f3ab7d9dbeada92d857fc5b07f232d5
Simon Chromow <simon@Simons-MacBook-Pro.local>
2016-05-24 14:26:56 +0200

    pushManager

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-(function() {
-    if ('serviceWorker' in navigator) {                
-        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+(function () {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js').then(function (registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
-          }).catch(function(err) {
+
+            registration.pushManager.subscribe().then(
+                function (pushSubscription) {
+                    console.log(JSON.stringify(pushSubscription));
+                }
+                , function (error) {
+                    console.log(error);
+                }
+            );
+
+        }).catch(function (err) {
             // registration failed :(
             console.log('ServiceWorker registration failed: ', err);
         });
