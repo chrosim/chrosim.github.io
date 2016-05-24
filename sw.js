c304cc907881200d85eb98cfd2e6603f724ebbc4
Simon Chromow <simon@Simons-MacBook-Pro.local>
2016-05-24 14:05:02 +0200

    recieve push

--- sw.js
+++ sw.js
@@ -1,8 +1,13 @@
 
-self.addEventListener('install', function(e) {  
-  console.log('[ServiceWorker] Install');  
-})
+self.addEventListener('install', function(e) {    
+    self.skipWaiting();
+    console.log('[ServiceWorker] Install');  
+});
                       
-self.addEventListener('activate', function(e) {  
-  console.log('[ServiceWorker] Activate'); 
-})
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate'); 
+});
+
+self.addEventListener('push', function(e){
+    console.log('[ServiceWorker] Push recieved: ' + JSON.stringify(e))
+});
