ef36f88d8dd33c842a5f4f75079b3b32377c5fb2
Simon Chromow <simon@Simons-MacBook-Pro.local>
2016-06-15 12:27:20 +0200

    blablabla

--- main.js
+++ main.js
@@ -4,8 +4,14 @@
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
 
-            registration.pushManager.subscribe().then(
+            registration.pushManager.subscribe({
+                userVisibleOnly: true
+            }).then(
                 function (pushSubscription) {
+                    fetch('http://localhost:8080/subscribe', {
+                        method: 'post'
+                        , body: JSON.stringify(pushSubscription)
+                    });
                     console.log(JSON.stringify(pushSubscription));
                 }
                 , function (error) {
@@ -19,3 +25,10 @@
         });
     }
 })();
+
+/*
+"{
+\"registration_ids\":[ \"e5xFKTVTbaY:APA91bHqE5ap_BAWUkUenecMmBooZQmKyMXbzEiG_Ts2Z_Cq9r65kB--qP89df51130yemZAZ5xeDyNyIz4qwNjdXMtuoHnHTgEPghwN2dtKVlXV7lOcCTMTHTQ52NUvkHvAW2xM583O\"],
+    \"data\": { \"name\": \"Hello Notification\"}
+}"
+*/
--- sw.js
+++ sw.js
@@ -1,13 +1,42 @@
-
-self.addEventListener('install', function(e) {    
+self.addEventListener('install', function (e) {
     self.skipWaiting();
-    console.log('[ServiceWorker] Install');  
-});
-                      
-self.addEventListener('activate', function(e) {
-    console.log('[ServiceWorker] Activate'); 
+    console.log('[ServiceWorker] Install');
 });
 
-self.addEventListener('push', function(e){
-    console.log('[ServiceWorker] Push recieved: ' + JSON.stringify(e))
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+});
+
+self.addEventListener('push', function (event, data) {
+    event.data = data;
+    console.log('[ServiceWorker] Push recieved: ', event);
+
+    event.waitUntil(
+        self.registration.showNotification('Push Notification recieved', {
+            body: 'This is the Message Body and it should be data but i do not recieve anything in push'
+            , icon: 'images/icon.png'
+            , tag: 'my-tag'
+        }));
+});
+
+self.addEventListener('notificationclick', function (event) {
+    console.log('Notification click: tag ', event.notification.tag);
+    event.notification.close();
+    var url = 'https://youtu.be/gYMkEMCHtJ4';
+    event.waitUntil(
+        clients.matchAll({
+            type: 'window'
+        })
+        .then(function (windowClients) {
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
 });
